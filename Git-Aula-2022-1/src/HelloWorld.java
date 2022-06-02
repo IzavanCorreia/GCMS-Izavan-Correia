5026d6ed2da0a053e2737eff4fcc81f96f984f26
Izavan Correia <isc1@discente.ifpe.edu.br>
2022-06-02 15:49:19 -0300

    Commit prática 04
    Commit prática 04 - parte 02

---
+++ Git-Aula-2022-1/src/HelloWorld.java
@@ -0,0 +1,20 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!!!");
+		System.out.println("Main um commit");
+		System.out.println("Commit BASH & ECLIPSE");
+		System.out.println("Commit from Willian");
+		System.out.println("Commit de Izavan");
+		System.out.println("Commit de José Alfredo");
+		System.out.println("Commit de willian da prática 4");
+		System.out.println("Modificação de Willian para pull request no fork do repo da turma prática 4");
+		System.out.println("Commit Lucas Pavão Pratica 4");
+		System.out.println("Modificação de Lucas Pavao para pull request no fork do repo da turma prática 4");
+		System.out.println("Commit de Bruno Holder");
+		System.out.println("Teste PUsh Chave");
+		System.out.println("Commit Izavan dos Santos Correia Pratica 4");
+	}
+	
+}
